5cc420069074ed1eaa7d20c7e54799fc47623872
RamonRS <celmon1@gmail.com>
2019-09-19 13:29:03 +0200

    Ejemplo1

---
+++ Tema 1/01EjemploDia2.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        System.Console.WriteLine("Es el recreo , nos vamos")
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("Debacle del Madrid en Champions")
+
+        Console.Title = "Ejemplo día 2"
+        Console.Beep(500, 2000)
+        Console.ReadLine()
+    End Sub
+
+End Module
