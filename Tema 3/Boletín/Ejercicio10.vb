a6310cc514f2348f7d2694f8af69040a5c47f9b7
RamonRS <celmon1@gmail.com>
2019-10-14 18:36:18 +0200

    j

---
+++ Tema 3/Boletín/Ejercicio10.vb
@@ -0,0 +1,29 @@
+﻿Module Ejercicio10
+    Sub Main()
+        Dim presion, temperatura As Double
+
+        Console.WriteLine("Introduzca la temperatura: ")
+        temperatura = Convert.ToDouble(Console.ReadLine())
+
+        Console.WriteLine("Introduzca la presión: ")
+        presion = Convert.ToDouble(Console.ReadLine())
+
+        If presion > 2 Then
+            Console.WriteLine("Abrir válvula de seguridad.")
+        End If
+
+        If temperatura > 500 Then
+            Console.WriteLine("Reducir la temperatura.")
+        End If
+
+        If presion < 2 And temperatura <= 500 Then
+            Console.WriteLine("Todo en orden.")
+        End If
+
+
+
+
+
+
+    End Sub
+End Module
