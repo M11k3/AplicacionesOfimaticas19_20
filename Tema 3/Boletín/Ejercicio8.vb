5c4fd94f08c0f7463887e1f2ee3315dc962b30c0
RamonRS <celmon1@gmail.com>
2019-10-14 16:43:26 +0200

    Boletín 3A Tema 3

--- Tema 3/Boletín/Ejercicio8.vb
+++ Tema 3/Boletín/Ejercicio8.vb
@@ -1,6 +1,24 @@
 ﻿
 Module Ejercicio8
     Sub Main()
+        Dim password1, password2 As String
+        password1 = "montecastelogmi1920"
+
+        'Solicitar password al usuario
+        Console.WriteLine("Introduzca su password")
+        password2 = Console.ReadLine()
+
+        If password1 = password2 Then
+            Console.WriteLine("Contraseña correcta , puede acceder")
+        Else
+            Console.Beep(450, 1000)
+            Console.WriteLine("LLamando al F.B.I.")
+        End If
+        Console.ReadLine()
+
+
+
+
 
     End Sub
 End Module
---
+++ Tema 3/Boletín/Ejercicio9.vb
@@ -0,0 +1,20 @@
+﻿Module Ejercicio9
+    Sub Main()
+        Dim n1, n2 As Integer
+        Dim potencia As Long
+
+        Console.WriteLine("Introduzca número 1:")
+        n1 = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Introduzca número 2:")
+        n2 = Convert.ToInt32(Console.ReadLine())
+
+        If n1 < n2 Then
+            potencia = Math.Pow(n2, n1)
+        Else
+            potencia = Math.Pow(n1, n2)
+        End If
+
+        Console.WriteLine("El más pequeño elevado al más grande es : " & potencia)
+        Console.ReadLine()
+    End Sub
+End Module
