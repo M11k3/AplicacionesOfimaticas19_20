9af0fd7b45c7ce43856bd4980d901f9fb44efd88
RamonRS <celmon1@gmail.com>
2019-11-28 13:17:31 +0100

    ejemplo3Comparacion

---
+++ Tema 5/03_ComparacionArrays.vb
@@ -0,0 +1,26 @@
+﻿Option Strict On
+Module Module1
+    Sub Main()
+        Dim m1() As Integer = {5, 4, 2, 1}
+        Dim m2() As Integer = {58, 42, 2, 1}
+        m1 = m2
+        'Si estos dos arrays son iguales
+
+        If m1 Is m2 Then
+            Console.WriteLine("Iguales")
+        Else
+            Console.WriteLine("Diferentes")
+        End If
+        Console.ForegroundColor = ConsoleColor.White
+        m2(0) = -890
+        m1(0) = 9000
+        Console.WriteLine("m1(0) : " & m1(0))
+        Console.WriteLine("m2(0) : " & m2(0))
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
