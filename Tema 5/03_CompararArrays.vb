5a64bc891ac6c7a2d44442c9150602571bdda0de
RamonRS <celmon1@gmail.com>
2019-12-04 09:36:51 +0100

    EjemposArrays

---
+++ Tema 5/03_CompararArrays.vb
@@ -0,0 +1,48 @@
+﻿Option Strict On
+Module Module1
+    Sub Main()
+        Dim m1() As Integer = {5, 4, 2, 1}
+        Dim m2() As Integer = {58, 42, 2, 1}
+        'Para saber si dos arrays son iguales debemos ir posición por posición
+        Dim distinto As Boolean = False
+        For i As Integer = 0 To m1.Length - 1 Step 1
+            If m1(i) <> m2(i) Then
+                distinto = True
+                Exit For
+            End If
+
+        Next
+
+
+
+
+        'Al igualar las dos referencias lo que hacemos es que m1 apunte a m
+        m1 = m2
+
+        'If m1 Is m2 Then
+        '    Console.WriteLine("Iguales")
+        'Else
+        '    Console.WriteLine("Diferentes")
+        'End If
+        Console.ForegroundColor = ConsoleColor.White
+        m2(0) = -890
+        m1(0) = 9000
+        Console.WriteLine("m1(0) : " & m1(0))
+        Console.WriteLine("m2(0) : " & m2(0))
+
+        'Algoritmo que me identifique si dos arrays son iguales
+        Dim x() As Integer = {1, 2, 3, 5, 0}
+        Dim y() As Integer = {6, 2, 4, 3, 1}
+
+
+
+
+
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
---
+++ Tema 5/04_ExplicacionForeach.vb
@@ -0,0 +1,60 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim alumnos() As String = {"Szymon", "Alexandre", "Carni", "Ucha", "Angel"}
+        Console.ForegroundColor = ConsoleColor.White
+        For i As Integer = 0 To alumnos.Length - 1 Step 1
+            Console.Write(alumnos(i) & " ")
+
+        Next
+        'Recorrer la matriz con un For Each
+        Console.WriteLine()
+        For Each alumno As String In alumnos
+            Console.Write(alumno & " ")
+
+        Next
+
+        Dim alumnoBuscado As String
+        Dim encontrado As Boolean = False
+        Console.WriteLine("Introduzca alumno que desea buscar:")
+        alumnoBuscado = Console.ReadLine()
+
+        For Each a As String In alumnos
+            If alumnoBuscado = a Then
+                Console.WriteLine("Alumno encontrado")
+                encontrado = True
+                Exit For
+
+            End If
+        Next
+
+        If Not encontrado Then
+            Console.WriteLine("Alumno no encontrado")
+        End If
+
+        Dim notas() As Integer = {3, 8, 3, 1, 2}
+
+        Dim suma As Integer = 0
+        Dim media As Double = 0
+
+        For Each n As Integer In notas
+            suma = suma + n
+        Next
+        media = suma / notas.Length
+
+
+
+
+
+        Console.WriteLine()
+        Console.ReadLine()
+
+
+
+
+
+
+    End Sub
+
+End Module
