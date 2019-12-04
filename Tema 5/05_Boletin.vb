1312cfdb0dd3b8eba99b8417b81b6a46f4b67fc7
RamonRS <celmon1@gmail.com>
2019-12-04 20:36:05 +0100

    Boletin Arrays

--- Tema 5/05_Boletin.vb
+++ Tema 5/05_Boletin.vb
@@ -1,9 +1,8 @@
-﻿Option Strict On
-Module Module1
+﻿Module Module1
 
     Sub Main()
+        Dim ejercicio As Integer = 10
 
-        Dim ejercicio As Integer = 3
 
         Select Case ejercicio
             Case 1
@@ -86,24 +85,181 @@ Module Module1
                     Console.WriteLine("Desviacion elemento(" & i & ") = " & desviacion)
                 Next
 
+
                 desviacionMedia = sumaDesviaciones / temperaturas.Length
 
                 Console.WriteLine("La desviacion media : " & desviacionMedia)
 
             Case 4
+                Dim n As Integer
+                'Solicitamos al usuario número de elementos
+                Console.WriteLine("Intro número elementos")
+                n = Convert.ToInt32(Console.ReadLine())
+                'Declaramos matriz de tamaño n
+                Dim matriz(n - 1) As Integer
 
+                'Solicitamos al usuario que introduzca datos y los guardamos en la matriz
+                For i As Integer = 0 To matriz.Length - 1 Step 1
+                    Console.WriteLine("Intro valor posición : " & i)
+                    matriz(i) = Convert.ToInt32(Console.ReadLine())
+                Next
 
+                'A continuación recorremos la matriz para comprobar si tiene números negativos
+                'Declaro una booleana para detectar si tiene o no tiene negativos
+                Dim tieneNegativos As Boolean = False
 
+                For i As Integer = 0 To matriz.Length - 1 Step 1
+                    If matriz(i) < 0 Then
+                        Console.WriteLine("El valor " & matriz(i) & " ocupa la posición : " & i)
+                        tieneNegativos = True
+                    End If
+                Next
 
+                If Not tieneNegativos Then
+                    Console.WriteLine("No tiene números negativoss")
+                End If
 
+            Case 5
+                Dim a1(4), a2(4) As Integer
 
+                'Solicitamos datos del primerp
+                For i As Integer = 0 To a1.Length - 1 Step 1
+                    Console.WriteLine("Intro valor posición : " & i)
+                    a1(i) = Convert.ToInt32(Console.ReadLine())
+                Next
 
+                'Volcamos datos del primero al segundo
 
+                For i As Integer = 0 To a1.Length - 1 Step 1
+                    a2(i) = a1(i) + 10
+                Next
 
+                'Mostramos ambos arrays por pantalla : Usando un For each
 
+                Console.WriteLine("Array 1")
+                For Each valor As Integer In a1
+                    Console.Write(valor & " ")
+                Next
+                Console.WriteLine()
+                Console.WriteLine("Array 2")
 
+                For Each valor As Integer In a2
+                    Console.Write(valor & " ")
+                Next
+                Console.WriteLine()
 
+            Case 6
+                Dim n As Integer
+                'Solicitamos al usuario número de elementos
+                Console.WriteLine("Intro número elementos")
+                n = Convert.ToInt32(Console.ReadLine())
+                'Declaramos matriz de tamaño n
+                Dim matriz(n - 1) As Integer
+                Dim sumaMayores15 As Integer = 0
 
+                'Solicitamos al usuario que introduzca datos y los guardamos en la matriz
+                For i As Integer = 0 To matriz.Length - 1 Step 1
+                    Console.WriteLine("Intro valor posición : " & i)
+                    matriz(i) = Convert.ToInt32(Console.ReadLine())
+                Next
+
+                'Calculamos suma de los > 15
+                'Mostramos los menores de 15 por pantalla
+                Console.WriteLine("Los elementos < 15 son : ")
+                For i As Integer = 0 To matriz.Length - 1 Step 1
+                    If matriz(i) > 15 Then
+                        sumaMayores15 += matriz(i)
+                    Else
+                        Console.WriteLine(matriz(i) & " posicion: " & i)
+                    End If
+                Next
+
+                Console.WriteLine("La suma de los > 15 es " & sumaMayores15)
+
+            Case 7
+                Dim vector() As Integer = {6, 4, 3}
+
+                Dim sumaCuadrados As Integer
+                Dim modulo As Double
+
+                For i As Integer = 0 To vector.Length - 1 Step 1
+                    sumaCuadrados = sumaCuadrados + vector(i) * vector(i)
+                Next
+
+                modulo = Math.Sqrt(sumaCuadrados)
+                Console.WriteLine("El módulo del vector es: " & modulo)
+
+            Case 8
+                Dim n As Integer
+                'Solicitamos al usuario número de elementos
+                Console.WriteLine("Intro número elementos")
+                n = Convert.ToInt32(Console.ReadLine())
+                'Declaramos matriz de tamaño n
+                Dim matriz(n - 1) As Integer
+                Dim sumaMayores15 As Integer = 0
+
+                'Solicitamos al usuario que introduzca datos y los guardamos en la matriz
+                For i As Integer = 0 To matriz.Length - 1 Step 1
+                    Console.WriteLine("Intro valor posición : " & i)
+                    matriz(i) = Convert.ToInt32(Console.ReadLine())
+                Next
+
+                Dim posicion As Integer
+                Console.WriteLine("Intro posición que desea intercambiar por último elemento")
+                posicion = Convert.ToInt32(Console.ReadLine())
+
+                'Mostramos matriz antes de intercambio
+                Console.WriteLine("Matriz introducida: ")
+                For Each numero As Integer In matriz
+                    Console.Write(numero & " ")
+                Next
+                Console.WriteLine()
+                Console.WriteLine(" *************   ")
+                'Intercambiamos posiciones
+                Dim memoria As Integer = matriz(posicion)
+                matriz(posicion) = matriz(matriz.Length - 1)
+                matriz(matriz.Length - 1) = memoria
+
+                'Mostramos matriz después de intercambio
+                Console.WriteLine("Matriz modificada: ")
+                For Each numero As Integer In matriz
+                    Console.Write(numero & " ")
+                Next
+
+            Case 9
+                Dim matriz() As Integer = {5, 6, 7, -3, 0, 9, 6}
+                Dim negativos As Boolean = False
+
+                For i As Integer = 0 To matriz.Length - 1 Step 1
+                    If matriz(i) < 0 Then
+                        negativos = True
+                        Console.WriteLine(matriz(i) & " posición : " & i)
+                        Exit For
+                    End If
+                Next
+
+                If Not negativos Then
+                    Console.WriteLine("No hay números negativos")
+                End If
+
+            Case 10
+                Dim valores() As Integer = {5, 20, 8, 4, 5, 7}
+                Dim valorBuscado As Integer
+                Dim encontrado As Boolean = False
+
+                Console.WriteLine("Introduzca el valor que desea buscar:")
+                valorBuscado = Convert.ToInt32(Console.ReadLine())
+                Console.WriteLine(valorBuscado & " está en las posiciones: ")
+                For i As Integer = 0 To valores.Length - 1 Step 1
+                    If valores(i) = valorBuscado Then
+                        encontrado = True
+                        Console.Write(i & " ")
+                    End If
+                Next
+                Console.WriteLine()
+                If Not encontrado Then
+                    Console.WriteLine("No se ha encontrado el valor buscado")
+                End If
 
 
 
@@ -121,17 +277,7 @@ Module Module1
 
 
         End Select
-
-
-
-
         Console.ReadLine()
-
-
-
-
-
-
 
     End Sub
 
