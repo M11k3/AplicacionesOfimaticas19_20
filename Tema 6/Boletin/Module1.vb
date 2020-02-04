c31cc02139bff258a3b720a2034a1e6aedddc8b1
RamonRS <celmon1@gmail.com>
2020-02-04 11:59:19 +0100

    Boletin

---
+++ Tema 6/Boletin/Module1.vb
@@ -0,0 +1,54 @@
+﻿Module Module1
+
+    Sub Main()
+        'Probar función ejercicio 2
+        Dim n As Integer
+        Console.WriteLine("Introduce un número")
+        n = Convert.ToInt32(Console.ReadLine())
+        Dim raiz As Double = raizCuadrada(n)
+        Console.WriteLine("La raíz cuadrad de " & n & " es " & raiz)
+        Console.WriteLine("Raiz con 2 decimales: " & raizCuadrada(n, 2))
+        Console.WriteLine("Raiz con 3 decimales: " & raizCuadrada(n, 3))
+
+        Console.WriteLine("El más pequeño es: " & minimo(8, 3))
+        Dim x As Integer = 9
+        Dim y As Integer = 39
+        Console.WriteLine("El más pequeño es: " & minimo(x, y))
+
+        'Probar función ejercicio 4: is Primo
+
+        If isPrimo(n) Then
+            Console.WriteLine("Es primo")
+        Else
+            Console.WriteLine("No es primo.")
+        End If
+
+
+        Dim matriz() As Integer = {5, 7, 9, 8, 7, 2, 1}
+
+        For Each numero As Integer In matriz
+            If isPrimo(numero) Then
+                e(numero & " es primo")
+            End If
+        Next
+
+        'Probar ejercicio 6
+        Dim n1 As Integer = 5
+        Dim n2 As Integer = 9
+        Console.WriteLine("n1 = " & n1)
+        Console.WriteLine("n2 = " & n2)
+        intercambiar(n1, n2)
+        Console.WriteLine()
+        e("intercambio: ")
+        e("n1 = " & n1)
+        e("n2 = " & n2)
+
+        e("probando ejercicio 7")
+
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Tema 6/Boletin/funciones.vb
@@ -0,0 +1,70 @@
+﻿Option Strict On
+Module Boletin2
+
+    Public Function raizCuadrada(ByVal n As Double) As Double
+
+        If n > 0 Then
+            Return Math.Sqrt(n)
+        Else
+            Return 0
+        End If
+
+    End Function
+
+    Public Function raizCuadrada(ByVal n As Double, ByVal decimales As Integer) As Double
+        If n > 0 Then
+            Return Math.Round(Math.Sqrt(n), decimales)
+        Else
+            Return 0
+        End If
+    End Function
+
+    Public Function minimo(ByVal x As Integer, ByVal y As Integer) As Integer
+        If x < y Then
+            Return x
+        Else
+            Return y
+        End If
+    End Function
+
+    Public Function minimo(ByVal x As Double, ByVal y As Double) As Double
+        If x < y Then
+            Return x
+        Else
+            Return y
+        End If
+    End Function
+
+    'Función ejercicio 4
+
+    Public Function isPrimo(ByVal n As Integer) As Boolean
+        Dim contador As Integer = 0
+
+        For i As Integer = 2 To Convert.ToInt32(n / 2) Step 1
+            If n Mod i = 0 Then
+                contador += 1
+                Exit For
+            End If
+        Next
+
+        If contador = 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    'Ejercicio 6
+
+    Public Sub intercambiar(ByRef x As Integer, ByRef y As Integer)
+        Dim auxiliar As Integer = x
+        x = y
+        y = auxiliar
+    End Sub
+
+    'ejercicio 7
+    Public Sub e(ByVal mensaje As String)
+        Console.WriteLine(mensaje)
+    End Sub
+
+End Module
