e77f2d51cac218272aa3f927e6f3dd968efe36e1
RamonRS <celmon1@gmail.com>
2020-03-10 12:18:42 +0100

    Excepciones

---
+++ Tema 7_Cadenas/07_Excepciones.vb
@@ -0,0 +1,35 @@
+﻿Option Strict On
+Imports System.IO
+Module Module1
+
+    Sub Main()
+        Dim ruta As String = Directory.GetCurrentDirectory() + "\datos.txt"
+        Dim registros(49) As String
+        Dim i As Integer = 0
+        Dim lector As StreamReader = New StreamReader(ruta)
+
+        While Not lector.EndOfStream
+            Dim registro As String = lector.ReadLine()
+            If registros(i) Is Nothing Then
+                registros(i) = registro
+                i = i + 1
+            End If
+
+        End While
+
+        lector.Close()
+
+        'Mostrar registros matriz
+
+        For Each valor As String In registros
+            If Not valor Is Nothing Then
+                Console.WriteLine(valor)
+            Else
+                Exit For
+            End If
+
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
